bfb2c6656e1c51686171f6575889b6e9e4f65b67
AbelTarazona <abel.tarazona@urp.edu.pe>
2018-12-09 22:49:59 -0500

    Prueba trayendo datos

--- BotNba/BotNba/Program.cs
+++ BotNba/BotNba/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using HtmlAgilityPack;
+using System.IO;
 
 namespace BotNba
 {
     class Program
     {
+        /*
+        1. Ver como obtener la info de jugadores
+        2. Guardar en carpeta un fichero por jugador
+        3. Guardar info en BD
+        4. Crear una web con los datos
+        */
         static void Main(string[] args)
         {
+            WebClient client = new WebClient();
+            HtmlDocument doc = new HtmlDocument();
+            HtmlNode.ElementsFlags["br"] = HtmlElementFlag.Empty;
+            doc.OptionWriteEmptyNodes = true;
+
+            var web = HttpWebRequest.Create("http://www.espn.com/nba/player/_/id/800");
+            Stream stream = web.GetResponse().GetResponseStream();
+            doc.Load(stream);
+
+            foreach(HtmlNode node in doc.DocumentNode.SelectNodes("//div[@class='player-bio']"))
+            {
+                Console.Write(node.InnerHtml);
+            }
+
+            Console.ReadLine();
         }
     }
 }
